b798545cbb135fa4d9e53efb943e9b7c9f5989ea
Thomas Ahlström <thomas.ahlstrom@student.forsbergsskola.se>
2021-05-27 11:52:41 +0200

    Adds copied code from C#

--- Nim-Game/Nim-Game/NimGame2.cpp
+++ Nim-Game/Nim-Game/NimGame2.cpp
@@ -11,10 +11,41 @@ static void NimGame2() {
 	char matchesASCII;
 	int player1MatchesDrawn = 0;
 	int player2MatchesDrawn = 0;
+	//create new random
 
 	cout << "Welcome to a game of Nim, don't draw the last match.\n";
 
-	for (int i = 0; i < matchesCurrent; i++)	{
-		matchesASCII += "|";
+	string MatchesDisplayASCII(int matchesCurrent) {
+		char matchesASCII;
+		matchesASCII = "";
+		for (int i = 0; i < matchesCurrent; i++) {
+			matchesASCII += "|";
+		}
+
+		cout << "Mathces left: " << matchesASCII << "\n";
+		return matchesASCII;
+	}
+
+	while (matchesCurrent > 0)	{
+		matchesASCII = MatchesDisplayASCII(matchesCurrent);
+
+		cout << "Player 1, please draw 1, 2 or 3 matches.\n";
+		cin >> player1MatchesDrawn;
+		matchesCurrent -= player1MatchesDrawn;
+
+		if (matchesCurrent == 0)		{
+			cout << "Player 1 lost.\n";
+			cout << "Game finished.\n";
+		}		else		{
+			MatchesDisplayASCII(matchesCurrent);
+			cout << "Player 2, please draw 1, 2 or 3 matches.\n";
+			//player2MatchesDrawn random
+			cout << player2MatchesDrawn;
+			matchesCurrent -= player2MatchesDrawn;
+			if (matchesCurrent == 0) {
+				cout << "Player 2 lost.\n";
+				cout << "Game finished.\n";
+			}
+		}
 	}
 }
